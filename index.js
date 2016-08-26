4f8e05fe74660703977227d570282d8c7b236db1
logiclogue <jordanpaullord@hotmail.co.uk>
2016-08-26 22:12:42 +0100

    Adds index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var Outputter = require('./src/Outputter.js');
+var SpriteSheet = require('./src/SpriteSheet.js');
+var Splitter = require('./src/Splitter.js');
+var Loader = require('./src/Loader.js');
+
+
+module.exports = {
+    Outputter: Outputter,
+    SpriteSheet: SpriteSheet,
+    Splitter: Splitter,
+    Loader: Loader
+};
