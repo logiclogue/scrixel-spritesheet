75ae1f311b9d71cf2e13cbb00f56e894dd9f86ee
logiclogue <jordanpaullord@hotmail.co.uk>
2017-12-01 00:18:34 +0000

    Updates to split into multiple columns

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-var Outputter = require('./src/Outputter.js');
-var SpriteSheet = require('./src/SpriteSheet.js');
-var Splitter = require('./src/Splitter.js');
-var Loader = require('./src/Loader.js');
+const Jimp = require("jimp");
+const _ = require("lodash");
 
+// Int -> Int -> String -> Promise [String]
+Jimp.read("./test/res/image.gif").then(image => {
+    const width = image.bitmap.width;
+    const height = image.bitmap.height;
 
-module.exports = {
-    Outputter: Outputter,
-    SpriteSheet: SpriteSheet,
-    Splitter: Splitter,
-    Loader: Loader
-};
+    let promise = new Promise((resolve, reject) => image.getBase64(Jimp.MIME_PNG, (x, y) => resolve(y)));
+
+    promise.then(console.log);
+
+    let values = columns(2, image).map(image => new Promise((resolve, reject) => image.getBase64(Jimp.MIME_PNG, (x, y) => resolve(y))));
+
+    Promise.all(values).then(console.log);
+}).catch(err => console.error(err));
+
+function columns(cols, image) {
+    const width = image.bitmap.width;
+    const height = image.bitmap.height;
+    const columnWidth = width / cols;
+
+    return _
+        .range(cols)
+        .map(i => i * columnWidth)
+        .map(x => image.clone().crop(x, 0, columnWidth, height));
+}
